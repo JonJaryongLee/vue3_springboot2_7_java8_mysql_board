6fc3933e05ba09d49595d5ad271415b3b3336553
JonJaryongLee <director9303@gmail.com>
2023-12-04 14:06:33 +0900

    add userPwdConfirm

--- client/src/views/SignupView.vue
+++ client/src/views/SignupView.vue
@@ -10,7 +10,34 @@ const router = useRouter();
 
 const userId = ref("");
 const userPwd = ref("");
+const userPwdConfirm = ref("");
 const userName = ref("");
+
+/**
+ * 입력된 아이디와 비밀번호의 유효성을 검사합니다.
+ *
+ * @function validateInput
+ * @returns {boolean} 입력된 아이디와 비밀번호가 유효하면 true를 반환하고, 그렇지 않으면 false를 반환합니다.
+ */
+ function validateInput() {
+  if (!userId.value || userId.value.length < 5) {
+    alert("아이디는 5자 이상이어야 합니다.");
+    return false;
+  }
+
+  const passwordRegex = /^(?=.*\d).{8,}$/;
+  if (!userPwd.value || !passwordRegex.test(userPwd.value)) {
+    alert("비밀번호는 8자 이상이며, 최소 하나의 숫자를 포함해야 합니다.");
+    return false;
+  }
+
+  if (userPwd.value !== userPwdConfirm.value) {
+    alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+    return false;
+  }
+
+  return true;
+}
 
 /**
  * 새로운 사용자를 비동기적으로 등록합니다.
@@ -21,15 +48,7 @@ const userName = ref("");
  * @throws {Error} 회원가입 작업이 실패하면 오류를 발생시킵니다.
  */
 async function signup() {
-  // 아이디와 비밀번호의 유효성 검사
-  if (!userId.value || userId.value.length < 5) {
-    alert("아이디는 5자 이상이어야 합니다.");
-    return;
-  }
-
-  const passwordRegex = /^(?=.*\d).{8,}$/;
-  if (!userPwd.value || !passwordRegex.test(userPwd.value)) {
-    alert("비밀번호는 8자 이상이며, 최소 하나의 숫자를 포함해야 합니다.");
+  if (!validateInput()) {
     return;
   }
 
@@ -75,6 +94,16 @@ async function signup() {
         <input
           id="userPwd"
           v-model="userPwd"
+          type="password"
+          class="form-control"
+          required
+        />
+      </div>
+      <div class="mb-3">
+        <label for="userPwdConfirm" class="form-label">비밀번호 확인:</label>
+        <input
+          id="userPwdConfirm"
+          v-model="userPwdConfirm"
           type="password"
           class="form-control"
           required
